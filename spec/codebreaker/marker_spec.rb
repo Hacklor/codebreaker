f10bfb0e2d7eee3c9e525672be02e4979ba883c5
Nicole Bekkers <nicole@finalist.nl>
2013-09-25 12:31:01 +0200

    Removed pending for refactor

--- spec/codebreaker/marker_spec.rb
+++ spec/codebreaker/marker_spec.rb
@@ -63,7 +63,6 @@ module Codebreaker
 
       context 'with 1 exact match duplicated in guess' do
         it 'returns 0' do
-          pending('refactor number_match_count')
           marker = Marker.new('1234', '1155')
           expect(marker.number_match_count).to eq(0)
         end
