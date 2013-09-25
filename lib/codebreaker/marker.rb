9c85f42eeff07b8e6fcf48732d23c3dfdcdcf2d5
Nicole Bekkers <nicole@finalist.nl>
2013-09-25 12:30:24 +0200

    Refactor of #number_match_count

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -16,9 +16,19 @@ module Codebreaker
     end
 
     def number_match_count
-      (0..3).inject(0) do |count, index|
-        count + (number_match?(index) ? 1 : 0)
+      total_match_count - exact_match_count
+    end
+
+    def total_match_count
+      count = 0
+      secret_code = secret.split('')
+      guess.split('').map do |n|
+        if secret_code.include?(n)
+          secret_code.delete_at(secret_code.index(n))
+          count += 1
+        end
       end
+      count
     end
 
     private
