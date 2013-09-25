812988535552fd699b424b1566035a19d53ba32d
Nicole Bekkers <nicole@finalist.nl>
2013-09-25 09:53:44 +0200

    Return mark when first number is included in secret

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -9,11 +9,20 @@ module Codebreaker
     end
 
     def start(secret)
+      @secret = secret
+
       output.puts 'Welcome to Codebreaker!'
       output.puts 'Enter guess:'
     end
 
     def guess(guess)
+      if @secret.include?(guess[0])
+        mark = '-'
+      else
+        mark = ''
+      end
+
+      output.puts mark
     end
   end
 
